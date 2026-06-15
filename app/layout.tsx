74b81b73efd3eb86a2a586688d293545fa50dda4
PREM RANJAN SHARMA <153382361+premranjansharma@users.noreply.github.com>
2026-06-15 16:58:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from '@/components/Footer';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +22,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+
+        <main className="grow">
+          {children}
+        </main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
